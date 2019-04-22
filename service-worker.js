05beb646ddad4a50e2b452e74470d6118e40132a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-22 18:20:21 +0000

    Deploy open-blog.dev to github.com/open-blog-initiative/open-blog.dev.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,11 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "d723d65dae0db9a2c58205ee6b8f269a"
+    "revision": "0195ec93ebdafbed5a3e4748bb0e3fd7"
   },
   {
-    "url": "assets/css/0.styles.1a24ba94.css",
-    "revision": "1509d0c0fecac9a094494585be9c2a81"
+    "url": "assets/css/0.styles.d3f2b45c.css",
+    "revision": "4f0e82ab1c2e3f16a79b9555a4708a50"
   },
   {
     "url": "assets/img/create-assignement.038e5207.png",
@@ -40,76 +40,84 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.23d9d773.js",
-    "revision": "1a1875d7ee27e6273df28136ec3dd259"
+    "url": "assets/js/10.4c020dd4.js",
+    "revision": "99c77ccb3ccc2727dc908ca404d2a610"
   },
   {
-    "url": "assets/js/11.7393ed31.js",
-    "revision": "714e046baf6ef6b3b063cddb65df9a71"
+    "url": "assets/js/11.57344d4c.js",
+    "revision": "9afb437a4a11767c13b437c235f68255"
   },
   {
-    "url": "assets/js/12.a59b86ae.js",
-    "revision": "7f8073074245871d305894cb0585f9b3"
+    "url": "assets/js/12.46481c2c.js",
+    "revision": "126ae7d54343547ba623841759b81674"
   },
   {
-    "url": "assets/js/13.38f2b8cc.js",
-    "revision": "b89b2adfe5fa3bad90bf2dfc1d23f860"
+    "url": "assets/js/13.a8e64980.js",
+    "revision": "79072a545a8ff8b4faad93ec15da3d40"
   },
   {
-    "url": "assets/js/3.5229ec25.js",
-    "revision": "52f98e94257ae3b5e0e923dcf3cc2d05"
+    "url": "assets/js/14.8099a3e8.js",
+    "revision": "d0e768d4cd53fb8a84f35e4b500df2be"
   },
   {
-    "url": "assets/js/4.e4af55aa.js",
-    "revision": "a8379385a37b289364ce2ee90011c494"
+    "url": "assets/js/15.23efa781.js",
+    "revision": "d6f3ed7105b579662494aad5dd1df983"
   },
   {
-    "url": "assets/js/5.026900ef.js",
-    "revision": "921aceed2b0535eba60ca78c96635939"
+    "url": "assets/js/3.d884a169.js",
+    "revision": "496496d0e49c6c903c7483e2a3f99e80"
   },
   {
-    "url": "assets/js/6.b610a564.js",
-    "revision": "35c1fd49c2c25be39de940d028c64f03"
+    "url": "assets/js/4.b68e15b3.js",
+    "revision": "e980b3f8f193167e75d942202abafd32"
   },
   {
-    "url": "assets/js/7.3684bb1d.js",
-    "revision": "5a23f2bb72cf565bcedfda7cdf7ef090"
+    "url": "assets/js/5.1ccc99c0.js",
+    "revision": "6e088614b259cf16db7024dbc7f46fef"
   },
   {
-    "url": "assets/js/8.3ecda600.js",
-    "revision": "994384e36e3812b2d3fefb6d800b955e"
+    "url": "assets/js/6.46d51126.js",
+    "revision": "93eff89a81c6cf227f3f0a3459835c27"
   },
   {
-    "url": "assets/js/9.c5a5e431.js",
-    "revision": "34c8462fddc3e9d530505de9ea39d342"
+    "url": "assets/js/7.5aa959e4.js",
+    "revision": "5e473e3ad4cf1a8e5fe3518e7667a1e8"
   },
   {
-    "url": "assets/js/app.cdb13cc7.js",
-    "revision": "3afd57063ef6eb52b065b9ad92510d06"
+    "url": "assets/js/8.9e68a46d.js",
+    "revision": "37e9ee53160e2d2957fb192dda9025be"
   },
   {
-    "url": "assets/js/vendors~docsearch.9d0c5001.js",
-    "revision": "08edd9a8fb28e9ba7c0d5086749e1d22"
+    "url": "assets/js/9.9bd3ea0c.js",
+    "revision": "1fb62d811e866d9d7873f496424a87cd"
+  },
+  {
+    "url": "assets/js/app.4f87c933.js",
+    "revision": "8feb028ab820a9a061ef258474ac0d13"
+  },
+  {
+    "url": "assets/js/vendors~docsearch.7824dab0.js",
+    "revision": "7e5a2fa12b550184fa31888baec90dc1"
   },
   {
     "url": "contributing.html",
-    "revision": "3c871bb0bfe5549c85a6253d5725d343"
+    "revision": "f21bf364e5d90f235b169c93e74e9161"
   },
   {
     "url": "fr/index.html",
-    "revision": "b3d5bb581ab399a701c4b0fba2a9fbd3"
+    "revision": "24e3d6bdb3c5fefccfc50cd811d8f9fb"
   },
   {
     "url": "fr/post/Slasgear/github-classroom-pourquoi-le-tester.html",
-    "revision": "868c709324c3b54046ffb6631cb7dc46"
+    "revision": "614cf5d0540e42c07d65064ddcf7dde0"
   },
   {
     "url": "index.html",
-    "revision": "c344b0d5ce0391c5419d274497ba9227"
+    "revision": "e2f7c77979a4031d36a98e0ac2229179"
   },
   {
     "url": "post/Slashgear/creating-an-image-lazy-loading-component-with-react.html",
-    "revision": "f8fa33874cc9306969961be891ec1cb4"
+    "revision": "bc09b5b3b3aca90557d2e5466f68af82"
   },
   {
     "url": "Slashgear/github-classroom-logo.jpg",
